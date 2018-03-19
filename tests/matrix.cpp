b89a8612421dfa7aec35ec71df80e743481521e5
BorisovSergei113 <borisov_sergei113@mail.ru>
2018-03-19 22:08:57 +0300

    change

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -1,13 +1,11 @@
-
 #include "catch.hpp"
 #include <sstream>
-
 #include "matrix.hpp"
 using namespace std;
 
 TEST_CASE("creating matrix")
 {
-    matrix_t<int> matrix;
+    matrix_t <int> matrix;
     REQUIRE( matrix.Get_Stroki() == 0 );
     REQUIRE( matrix.Get_Stolbi() == 0 );
 }
@@ -25,6 +23,28 @@ TEST_CASE("reading matrix")
     REQUIRE( matrix.read( istream ) );
     REQUIRE( matrix.Get_Stroki()== 3 );
     REQUIRE( matrix.Get_Stolbi() == 3 );
+    
+    ostringstream ostream;
+    matrix.write( ostream );
+    
+    REQUIRE( input == ostream.str() );
+}
+
+TEST_CASE("= matrix")
+{
+    string input{
+        "3,3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    matrix_t<int> matrix;
+    istringstream istream{ input };
+    REQUIRE( matrix.read( istream ) );
+    matrix_t<int> copy;
+    copy=matrix;
+    REQUIRE( copy.Get_Stroki()== 3 );
+    REQUIRE( copy.Get_Stolbi() == 3 );
+    REQUIRE_NOTHROW( matrix.Get_Stolbi() == copy.Get_Stolbi() && matrix.Get_Stroki() == copy.Get_Stroki() );
     
     ostringstream ostream;
     matrix.write( ostream );
@@ -58,9 +78,21 @@ TEST_CASE("add matrix")
     
     matrix1.read(istream1);
     matrix2.read(istream2);
+    
+    REQUIRE( matrix1.Get_Stroki()== 3 );
+    REQUIRE( matrix1.Get_Stolbi() == 3 );
+    
+    REQUIRE( matrix2.Get_Stroki()== 3 );
+    REQUIRE( matrix2.Get_Stolbi() == 3 );
+    
     matrix3=matrix1+matrix2;
+    
     REQUIRE( matrix3.Get_Stroki()== 3 );
     REQUIRE( matrix3.Get_Stolbi() == 3 );
+    
+    
+    
+    REQUIRE_NOTHROW( matrix1.Get_Stolbi() == matrix2.Get_Stolbi() && matrix1.Get_Stroki() == matrix2.Get_Stroki() );
     
     ostringstream ostream;
     matrix3.write( ostream );
@@ -71,24 +103,24 @@ TEST_CASE("a-b matrix")
 {
     string input1{
         "3,3\n"
-        "1 1 1\n"
-        "2 2 2\n"
-        "3 3 3" };
-    matrix_t<int> matrix1;
+        "1.1 1.1 1.1\n"
+        "2.2 2.2 2.2\n"
+        "3.3 3.3 3.3" };
+    matrix_t<float> matrix1;
     istringstream istream1{ input1 };
     string input2{
         "3,3\n"
         "4 4 4\n"
         "5 5 5\n"
         "2 2 2" };
-    matrix_t<int> matrix2;
+    matrix_t<float> matrix2;
     istringstream istream2{ input2 };
     string input3{
         "3,3\n"
-        "-3 -3 -3\n"
-        "-3 -3 -3\n"
-        "1 1 1" };
-    matrix_t<int> matrix3;
+        "-2.9 -2.9 -2.9\n"
+        "-2.8 -2.8 -2.8\n"
+        "1.3 1.3 1.3" };
+    matrix_t<float> matrix3;
     istringstream istream3{ input3 };
     
     matrix1.read(istream1);
@@ -96,6 +128,14 @@ TEST_CASE("a-b matrix")
     matrix3=matrix1-matrix2;
     REQUIRE( matrix3.Get_Stroki()== 3 );
     REQUIRE( matrix3.Get_Stolbi() == 3 );
+    
+    REQUIRE( matrix1.Get_Stroki()== 3 );
+    REQUIRE( matrix1.Get_Stolbi() == 3 );
+    
+    REQUIRE( matrix2.Get_Stroki()== 3 );
+    REQUIRE( matrix2.Get_Stolbi() == 3 );
+    
+    REQUIRE_NOTHROW( matrix1.Get_Stolbi() == matrix2.Get_Stolbi() && matrix1.Get_Stroki() == matrix2.Get_Stroki() );
     
     ostringstream ostream;
     matrix3.write( ostream );
@@ -128,9 +168,19 @@ TEST_CASE("a*b matrix")
     
     matrix1.read(istream1);
     matrix2.read(istream2);
+    
+    REQUIRE( matrix1.Get_Stroki()== 3 );
+    REQUIRE( matrix1.Get_Stolbi() == 3 );
+    
+    REQUIRE( matrix2.Get_Stroki()== 3 );
+    REQUIRE( matrix2.Get_Stolbi() == 1);
+    
     matrix3=matrix1*matrix2;
+    
     REQUIRE( matrix3.Get_Stroki()== 3 );
     REQUIRE( matrix3.Get_Stolbi() == 1 );
+    
+    REQUIRE_NOTHROW( matrix1.Get_Stolbi() == matrix2.Get_Stroki() );
     
     ostringstream ostream;
     matrix3.write( ostream );
@@ -168,7 +218,7 @@ TEST_CASE("a*=b matrix")
     
     ostringstream ostream;
     matrix1.write( ostream );
-    
+    REQUIRE_NOTHROW( matrix1.Get_Stolbi() == matrix2.Get_Stroki() );
     REQUIRE( input3 == ostream.str());
 }
 
@@ -176,26 +226,29 @@ TEST_CASE("a+=b matrix")
 {
     string input1{
         "3,3\n"
-        "1 1 1\n"
-        "2 2 2\n"
-        "3 3 3" };
-    matrix_t<int> matrix1;
+        "1.1 1.1 1.1\n"
+        "2.1 2.1 2.1\n"
+        "3.1 3.1 3.1" };
+    matrix_t<float> matrix1;
     istringstream istream1{ input1 };
     string input2{
         "3,3\n"
         "4 6 1\n"
         "5 3 4\n"
         "2 7 3" };
-    matrix_t <int>matrix2;
+    matrix_t <float>matrix2;
     istringstream istream2{ input2 };
     string input3{
         "3,3\n"
-        "5 7 2\n"
-        "7 5 6\n"
-        "5 10 6" };
+        "5.1 7.1 2.1\n"
+        "7.1 5.1 6.1\n"
+        "5.1 10.1 6.1" };
     
     matrix1.read(istream1);
     matrix2.read(istream2);
+    
+    REQUIRE_NOTHROW( matrix1.Get_Stolbi() == matrix2.Get_Stolbi() && matrix1.Get_Stroki() == matrix2.Get_Stroki() );
+    
     matrix1+=matrix2;
     REQUIRE( matrix1.Get_Stroki()== 3 );
     REQUIRE( matrix1.Get_Stolbi() == 3 );
@@ -230,6 +283,9 @@ TEST_CASE("a-=b matrix")
     
     matrix1.read(istream1);
     matrix2.read(istream2);
+    
+    REQUIRE_NOTHROW( matrix1.Get_Stolbi() == matrix2.Get_Stolbi() && matrix1.Get_Stroki() == matrix2.Get_Stroki() );
+    
     matrix1-=matrix2;
     REQUIRE( matrix1.Get_Stroki()== 3 );
     REQUIRE( matrix1.Get_Stolbi() == 3 );
@@ -239,3 +295,4 @@ TEST_CASE("a-=b matrix")
     
     REQUIRE( input3 == ostream.str());
 }
+
